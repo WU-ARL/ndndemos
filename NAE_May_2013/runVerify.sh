fb2625f567a61a76159b1591f2e61f09b7b950f3
John DeHart <jdd@naedemo.(none)>
2013-04-18 16:54:17 -0500

    add runVerify.sh script

---
+++ NAE_May_2013/runVerify.sh
@@ -0,0 +1,53 @@
+#! /bin/bash
+
+cd verify_content
+echo $PWD
+ls
+
+SEQ=$RANDOM
+NUM=1
+TIMEOUT=3
+while (true)
+do
+
+  echo "0: Quit"
+  echo "1: for content signed by verified key"
+  echo "2: for content signed by unverified key using vefified key name"
+  echo "3: for content signed by corrupted key"
+  echo "9: repeat last request"
+
+
+  read x
+  if [ $x -eq 0 ]
+  then
+    exit
+  fi
+  if [ $x -eq 1 ]
+  then
+echo "$PWD"
+    echo "Sending interest for content signed by verified key"
+    PREFIX="ccnx:/ndn/wustl.edu/ndndemo/good_key/nae$SEQ/$NUM"
+    ./verify_content_client.py -t $TIMEOUT ccnx:/ndn/wustl.edu/ndndemo/good_key/nae$SEQ/$NUM
+    echo ""
+  fi
+  if [ $x -eq 2 ]
+  then
+    echo "Sending interest for content signed by unverified key using verified keyname"
+    PREFIX="ccnx:/ndn/wustl.edu/ndndemo/bad_key/nae$SEQ/$NUM"
+    ./verify_content_client.py -t $TIMEOUT ccnx:/ndn/wustl.edu/ndndemo/bad_key/nae$SEQ/$NUM
+  fi
+  if [ $x -eq 3 ]
+  then
+    echo "Sending interest for content signed by verified key, key was corrupted in key repo"
+    PREFIX="ccnx:/ndn/wustl.edu/ndndemo/cbad_key/nae$SEQ/$NUM"
+    ./verify_content_client.py -t $TIMEOUT ccnx:/ndn/wustl.edu/ndndemo/cbad_key/nae$SEQ/$NUM
+  fi
+  if [ $x -eq 9 ]
+  then
+    echo "Re-sending last interest "
+    ./verify_content_client.py -t $TIMEOUT $PREFIX
+  fi
+
+  NUM=$(($NUM + 1))
+
+done
