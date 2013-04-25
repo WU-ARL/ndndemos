018b6aabd4915b031f5c1495a5f126077fd67ddc
John DeHart <jdd@ndndemo.arl.wustl.edu>
2013-04-25 16:22:32 -0500

    Add directory of files to initialize repo

---
+++ NAE_May_2013/repo/initializeRepo.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+ccnputfile -local /ndn/wustl.edu/ndndemo/WU_Tour.html WU_Tour.html 
+ccnputfile -local /ndn/wustl.edu/ndndemo/WU_logo.png WU_logo.png 
+
+ccnputfile -local /ndn/wustl.edu/ndndemo/Anheuser_Busch.jpg Anheuser_Busch.jpg 
+ccnputfile -local /ndn/wustl.edu/ndndemo/Crow_Observatory.jpg Crow_Observatory.jpg 
+ccnputfile -local /ndn/wustl.edu/ndndemo/Brookings.jpg Brookings.jpg 
+ccnputfile -local /ndn/wustl.edu/ndndemo/Graham_Chapel.jpg Graham_Chapel.jpg 
+ccnputfile -local /ndn/wustl.edu/ndndemo/Holmes_Lounge.jpg Holmes_Lounge.jpg 
+ccnputfile -local /ndn/wustl.edu/ndndemo/Quad_at_Night.jpg Quad_at_Night.jpg 
+ccnputfile -local /ndn/wustl.edu/ndndemo/Eads_in_Quad.jpg Eads_in_Quad.jpg 
+ccnputfile -local /ndn/wustl.edu/ndndemo/Earth_and_Planetary_Science.jpg Earth_and_Planetary_Science.jpg 
+
+
+
+
+
+
